11678acbdccce7ffb8e9298864197a12f9248411
aliyeysides <ali.yeysides@gmail.com>
2015-01-10 11:26:48 -0500

    add project and projects routes and templates

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -4,5 +4,6 @@ EmberRailsBlog.Router.map(function() {
 	this.resource('index', {path: '/'});
 	this.resource('navbar', function(){
 	  this.resource('posts');
+	  this.resource('projects');
 	})
 });
---
+++ app/assets/javascripts/routes/project_route.js
@@ -0,0 +1,5 @@
+// For more information see: http://emberjs.com/guides/routing/
+
+EmberRailsBlog.ProjectRoute = Ember.Route.extend({
+
+});
