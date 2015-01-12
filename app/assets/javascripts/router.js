8b1d000b59713868644d00119e485ce20c8a00d5
aliyeysides <ali.yeysides@gmail.com>
2015-01-12 07:14:11 -0500

    add post resource in router, create dynamic segment = title

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -2,7 +2,9 @@
 
 EmberRailsBlog.Router.map(function() {
 	this.resource('index', {path: '/'}, function(){
-	  this.resource('posts');
+	  this.resource('posts', function(){
+	  	this.resource('post' {path: '/:title'})
+	  });
 	  this.resource('projects');
 	})
 });
