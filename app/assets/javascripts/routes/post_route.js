239d4713325e7db75e7bacdd3fb96a1322327125
aliyeysides <ali.yeysides@gmail.com>
2015-01-12 07:11:57 -0500

    create hook in post model

--- app/assets/javascripts/routes/post_route.js
+++ app/assets/javascripts/routes/post_route.js
@@ -1,5 +1,7 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 EmberRailsBlog.PostRoute = Ember.Route.extend({
-
+	model: function(params){
+		return this.store.findby('title', params.title)
+	}
 });
