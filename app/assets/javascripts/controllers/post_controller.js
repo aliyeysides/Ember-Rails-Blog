34e375e7c408a86fba63d0d1c6f94124fb89cf9d
aliyeysides <ali.yeysides@gmail.com>
2015-01-14 14:54:00 -0500

    add post controller, write functions that turn editing flag on and off

---
+++ app/assets/javascripts/controllers/post_controller.js
@@ -0,0 +1,13 @@
+EmberRailsBlog.PostController = Ember.ObjectController.extend({
+	isEditing: false,
+
+	actions: {
+		edit: function(){
+			this.set('isEditing', true)
+		},
+
+		doneEditing: function(){
+			this.set('isEditing', false)
+		}
+	}
+})
