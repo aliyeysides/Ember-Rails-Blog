b7bbf6ed663bff3f2322d1558be936f5cb27b2ab
aliyeysides <ali.yeysides@gmail.com>
2015-01-16 16:25:02 -0500

    remove editing option on post handlebar

--- app/assets/javascripts/controllers/post_controller.js
+++ app/assets/javascripts/controllers/post_controller.js
@@ -1,13 +1,3 @@
 EmberRailsBlog.PostController = Ember.ObjectController.extend({
-	isEditing: false,
 
-	actions: {
-		edit: function(){
-			this.set('isEditing', true)
-		},
-
-		doneEditing: function(){
-			this.set('isEditing', false)
-		}
-	}
 })
