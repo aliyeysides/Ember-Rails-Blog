75ee8473e3fb0b70921eec446ddccb945c450f47
aliyeysides <ali.yeysides@gmail.com>
2015-01-18 13:50:21 -0500

    refactor router

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -2,9 +2,11 @@
 
 EmberRailsBlog.Router.map(function() {
 	this.resource('index', {path: '/'}, function(){
-	  this.resource('posts');
-  	this.resource('post', {path: 'posts/:title'})
-	  this.resource('projects');
-	  this.resource('project', {path: 'projects/:name'})
-	})
+	  this.resource('posts', {path: '/posts'}, function(){
+	  	this.resource('post', {path: '/:title'});
+	  });
+	  this.resource('projects', {path: '/projects'}, function(){
+		  this.resource('project', {path: '/:name'})
+		});
+	});
 });
