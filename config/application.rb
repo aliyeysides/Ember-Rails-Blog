47979461fb05f64d508c27b2ea1cb16fa923e3c8
aliyeysides <ali.yeysides@gmail.com>
2015-01-21 14:46:37 -0500

    config ember variants in dev and production enviornments

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ Bundler.require(:default, Rails.env)
 
 module EmberRailsBlog
   class Application < Rails::Application
+    config.ember.variant = :production
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
