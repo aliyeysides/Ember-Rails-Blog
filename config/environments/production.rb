8570bf2de86c90a77b71a9bbd39a498d51502e0f
aliyeysides <ali.yeysides@gmail.com>
2015-01-21 14:42:52 -0500

    add config.ember.variant to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ EmberRailsBlog::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
-
+  config.ember.variant = :production
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
